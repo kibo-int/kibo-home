21b94d47a57c339c2649192571b8f741282a765d
EricSSartorius <EricSSartorius@gmail.com>
2018-02-25 15:22:59 -0700

    removes styling from 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Section } from '../../static/styles/utils';
-import { H2, Text } from '../../static/styles/typography';
-
 
 const NotFoundPage = () => (
-  <Section>
-    <H2 orange>NOT FOUND</H2>
-    <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
-  </Section>
+  <div>
+    <h2>NOT FOUND</h2>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
 );
 
 export default NotFoundPage;
