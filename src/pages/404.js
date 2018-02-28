f15acff3dd32a7f6a15502bc5b2f1688384aeb63
EricSSartorius <EricSSartorius@gmail.com>
2018-02-27 18:38:42 -0700

    updates 404 style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Section } from '../styles/utils';
+
 import { H2, Text } from '../styles/typography';
 
 const NotFoundPage = () => (
-  <Section>
+  <div>
     <H2 orange>NOT FOUND</H2>
     <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
-  </Section>
+  </div>
 );
 
 export default NotFoundPage;
